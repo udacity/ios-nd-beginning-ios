d368fba740437b5c4c4d2d5e6bf224804a71b883
Jarrod Parkes <jarrod@udacity.com>
2016-09-22 15:02:02 -0500

    use first param name for all functions 1 examples

--- Playground Collection/Part 2 - Robot Maze 2/Functions 1/Functions 1.playground/Pages/Default Params.xcplaygroundpage/Contents.swift
+++ Playground Collection/Part 2 - Robot Maze 2/Functions 1/Functions 1.playground/Pages/Default Params.xcplaygroundpage/Contents.swift
@@ -8,7 +8,8 @@ func calculatePriceForMealWithTip(priceOfMeal: Double, tipPercentage: Double = 0
 
 // This call uses the default tip of 15% (0.15)
 calculatePriceForMealWithTip(priceOfMeal: 43.27)
+calculatePriceForMealWithTip(priceOfMeal: 100.32, tipPercentage: 0.20)
+calculatePriceForMealWithTip(priceOfMeal: 65.43)
+calculatePriceForMealWithTip(priceOfMeal: 22.18)
 
-calculatePriceForMealWithTip(priceOfMeal: 43.27, tipPercentage: 0.20)
-calculatePriceForMealWithTip(priceOfMeal: 43.27, tipPercentage: 0.25)
 //: [Next](@next)
