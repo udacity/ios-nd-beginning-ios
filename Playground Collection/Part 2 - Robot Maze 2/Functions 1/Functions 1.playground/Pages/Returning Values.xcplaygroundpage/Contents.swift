b4795b116e9671bb6b46ccddd780d15153232085
Jarrod Parkes <jarrod@udacity.com>
2016-09-22 14:39:55 -0500

    use first param name for all functions 1 examples

--- Playground Collection/Part 2 - Robot Maze 2/Functions 1/Functions 1.playground/Pages/Returning Values.xcplaygroundpage/Contents.swift
+++ Playground Collection/Part 2 - Robot Maze 2/Functions 1/Functions 1.playground/Pages/Returning Values.xcplaygroundpage/Contents.swift
@@ -2,16 +2,16 @@
 /*:
 ## Returning Values
 */
-func calculateTip(_ priceOfMeal: Double) -> Double {
+func calculateTip(priceOfMeal: Double) -> Double {
     return priceOfMeal * 0.15
 }
 
 let priceOfMeal = 43.27
 
-let tip = calculateTip(priceOfMeal)
+let tip = calculateTip(priceOfMeal: priceOfMeal)
 
 let totalPriceForMeal = priceOfMeal + tip
 
-let totalPriceForMealInline = priceOfMeal + calculateTip(priceOfMeal)
+let totalPriceForMealInline = priceOfMeal + calculateTip(priceOfMeal: priceOfMeal)
 
 //: [Next](@next)
