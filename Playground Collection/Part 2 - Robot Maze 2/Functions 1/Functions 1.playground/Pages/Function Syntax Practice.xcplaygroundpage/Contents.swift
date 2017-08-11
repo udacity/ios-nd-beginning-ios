8bdd4b03d23bc0d379a5fa30ec720fc424133249
Dani Springer <musicbyds@icloud.com>
2017-08-11 12:18:43 +0200

    Udacity course

--- Playground Collection/Part 2 - Robot Maze 2/Functions 1/Functions 1.playground/Pages/Function Syntax Practice.xcplaygroundpage/Contents.swift
+++ Playground Collection/Part 2 - Robot Maze 2/Functions 1/Functions 1.playground/Pages/Function Syntax Practice.xcplaygroundpage/Contents.swift
@@ -5,5 +5,7 @@
 //: ### Exercise 1
 //: Define a function called `myFirstFunction` that takes 0 parameters and prints something.
 //: **The solution is available on the next page!**
-
+func myFirstFunction() {
+    print("Hello")
+}
 //: [Next](@next)
