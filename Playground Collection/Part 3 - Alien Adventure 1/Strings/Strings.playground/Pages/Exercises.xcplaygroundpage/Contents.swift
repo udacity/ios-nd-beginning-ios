e3b7c92d034f4f2d1d0a422bad3669a4a01645ef
Dani Springer <musicbyds@icloud.com>
2017-09-10 05:22:50 +0200

    course

--- Playground Collection/Part 3 - Alien Adventure 1/Strings/Strings.playground/Pages/Exercises.xcplaygroundpage/Contents.swift
+++ Playground Collection/Part 3 - Alien Adventure 1/Strings/Strings.playground/Pages/Exercises.xcplaygroundpage/Contents.swift
@@ -5,20 +5,23 @@ import Foundation
 //: ### Exercise 1
 //: Recreate the shoutString by using the didYouKnowString as a stem.
 let didYouKnowString = "Did you know that the Swift String class comes with lots of useful methods?"
-let whisperString = "psst" + ", " + didYouKnowString.lowercaseString
-let shoutString =  "HEY! DID YOU KNOW THAT THE SWIFT STRING CLASS COMES WITH LOTS OF USEFUL METHODS?"
+let whisperString = "psst" + ", " + didYouKnowString.lowercased()
+let shoutString =  whisperString.uppercased()
 
 //: ### Exercise 2
 //: How many characters are in this string?
 let howManyCharacters = "How much wood could a woodchuck chuck if a woodchuck could chuck wood?"
+howManyCharacters.characters.count
 
 //: ### Exercise 3
 //: Write a program that tells you whether or not this string contains the substring "ham".
-var word = "shampoo"
+var word = "bounce"
+word.contains("ounce")
 
 //: ### Exercise 4
 //: Write a program that deletes all occurrences of the word "like" in the following string.
 let lottaLikes = "If like, you wanna learn Swift, like, you should build lots of small apps, cuz it's like, a good way to practice."
+let lessLikes = lottaLikes.replacingOccurrences(of: "like", with: "")
 
 //: ### Exercise 5
 //: Mia is moving in to a new apartment and she needs to pay first and last month's rent plus a security deposit. Use string interpolation to replace the word "total" with the correct total.
