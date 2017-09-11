a7861e0c7efa305cae6255f01c8b2240963fcc05
Dani Springer <musicbyds@icloud.com>
2017-09-11 03:12:04 +0200

    course

--- Playground Collection/Part 3 - Alien Adventure 1/Optionals/Optionals.playground/Pages/About Nil Values.xcplaygroundpage/Contents.swift
+++ Playground Collection/Part 3 - Alien Adventure 1/Optionals/Optionals.playground/Pages/About Nil Values.xcplaygroundpage/Contents.swift
@@ -8,30 +8,31 @@ var x: Int
 
 // Object references can't store nil values either
 var c: AnyObject
-c = UIColor.redColor()
+c = UIColor.red
 //c = nil
+//var nada = nil
 
 //: ### Cool! Except, sometimes we need nil values.
 //: 1. A method that cannot return a value
-var y: Int
+var y: Int?
 var s1: String
 var s2: String
 
 s1 = "123"
 s2 = "ABC"
 
-//y = Int(s1)
-//y = Int(s2)
+y = Int(s1)
+y = Int(s2)
 
 //: 2. Properties that cannot be initialized when an object is constructed
 class ViewController: UIViewController {
-    //var button: UIButton
+    var button: UIButton?
 }
 
 //: ### Optionals can be passed as parameters to functions
 
 // Example: Picking up groceries in an optional car
-func pickUpGroceries(car:Car?) {
+func pickUpGroceries(car: Car?) {
     if let car = car {
         print("We'll pick up the groceries in the \(car.model)")
     } else {
@@ -40,9 +41,9 @@ func pickUpGroceries(car:Car?) {
 }
 
 var someCar = Car(make: "Toyota", model: "Corolla")
-pickUpGroceries(someCar)
+pickUpGroceries(car: someCar)
 
-pickUpGroceries(nil)
+pickUpGroceries(car: nil)
 
 // Example: Hosting a guest in an optional extra room
 func host(guest: String, extraRoom: Room?) {
@@ -54,5 +55,5 @@ func host(guest: String, extraRoom: Room?) {
 }
 
 var someRoom = Room(name: "guest room", occupied: false)
-host("Grandma", extraRoom: someRoom)
-host("Ryan", extraRoom: nil)
+host(guest: "Grandma", extraRoom: someRoom)
+host(guest: "Ryan", extraRoom: nil)
