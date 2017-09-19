5af1de28fee1b0a5c33bedc5452cd36ced42af8a
Dani Springer <musicbyds@icloud.com>
2017-09-19 18:58:34 +0200

    Course

--- Playground Collection/Part 3 - Alien Adventure 1/Loops/Loops.playground/Pages/For-Each Loops.xcplaygroundpage/Contents.swift
+++ Playground Collection/Part 3 - Alien Adventure 1/Loops/Loops.playground/Pages/For-Each Loops.xcplaygroundpage/Contents.swift
@@ -2,18 +2,28 @@
 ## For-Each Loops
 A for-each loop performs an action for each item in a sequence, like a range.
 */
+import Foundation
+
+var lst = [Any]()
 for i in 1...10 {
+    if i % 2 == 0 {
+        lst.append(String(i) + " is even")
+    } else {
+        lst.append(String(i) + " is odd")
+    }
+}
+print(lst)
+
+for i in 1...3 {
+    print("Three cheers for Swift!")
     print(i)
 }
 
-for _ in 1...3 {
-    print("Three cheers for Swift!")
-}
-
 let word = "Mississippi"
-for c in word.characters {
+for c in word.unicodeScalars {
     print(c)
 }
+
 
 for c in "Oh hi, doggie 🐕".characters {
     print(c)
