98b6e7c8b8f0e1572e52b10c13ad292ea726428d
Dani Springer <musicbyds@icloud.com>
2017-09-24 22:41:38 +0200

    Course

--- Playground Collection/Part 3 - Alien Adventure 1/Arrays/Arrays.playground/Pages/Three ways to initialize an array.xcplaygroundpage/Contents.swift
+++ Playground Collection/Part 3 - Alien Adventure 1/Arrays/Arrays.playground/Pages/Three ways to initialize an array.xcplaygroundpage/Contents.swift
@@ -6,6 +6,8 @@ var numbers = Array<Double>()
 
 // More often you will see ...
 var moreNumbers = [Double]()
+moreNumbers.append(2)
+print(moreNumbers)
 
 // Array literal syntax
 let differentNumbers = [97.5, 98.5, 99.0]
